9cd0f4902874c617f490f818f507b5a7117eff03
Saidbeg-I <saido1996@mail.ru>
2023-01-29 22:58:08 +1200

    Initial Commit

---
+++ ViewColorDye/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewColorDye
+//
+//  Created by 1 on 29.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
